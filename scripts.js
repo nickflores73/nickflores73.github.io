b13d36e264e5b6c8d8066c1ac54024c963c91d9b
nickflores73 <46428751+nickflores73@users.noreply.github.com>
2019-01-06 15:33:28 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,5 +10,5 @@ var string9 = "emiumresponsivewebdesignstartupfoundersleanstartupsocialproofanal
 var string10 = "optersscrumprojectbetatermsheetvalidationvaluepropositionfundingstealthstartupstockpitchparadigmshiftalphastrategyventurenichemarketassetsadvisordeploymentchannelshypothesesstockholygrailstealthboots";
 var string11 = "trapingmonetization";
 if (window.location.href  === "https://nickflores73.github.io/adpage/ads.html") {
-  window.history.pushState("string", "Title", "http://localhost/adpage/ads.html?" + string1 + string2 + string3 + string4 + string5 + string6 + string7 + string8 + string9  + string10 + string11);
+  window.history.pushState("string", "Title", "https://nickflores73.github.io/adpage/ads.html?" + string1 + string2 + string3 + string4 + string5 + string6 + string7 + string8 + string9  + string10 + string11);
 }
