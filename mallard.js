b7c6e00378d0f6f2f48802df8d0a89aa6d3ffed7
nflore02 <nicholas.flores@tufts.edu>
2019-09-19 15:04:23 -0700

    my ish now

---
+++ mallard.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $path5 = $('.path5');
+  $path6 = $('.path6');
+  $path7 = $('.path7');
+  $path8 = $('.path8');
+
+  $( "svg:nth-child(5)" ).mouseover(function() {
+    console.log("hello");
+  });
+
+});
