1f487f817f12752f03bf9d08bc3618b32a278975
nickflores73 <46428751+nickflores73@users.noreply.github.com>
2020-01-15 14:05:54 -0800

    Add files via upload

--- mallard.js
+++ mallard.js
@@ -5,8 +5,23 @@ $(document).ready(function(){
   $path7 = $('.path7');
   $path8 = $('.path8');
 
-  $( "svg:nth-child(5)" ).mouseover(function() {
-    console.log("hello");
+  $roto = $('.roto');
+  $geared = $('.geared');
+  $textpop = $('.textpop');
+
+  $geared.css('transform', 'scale(1,1)');
+  $roto.mouseenter(function() {
+    $(this).css('transform', 'rotate(90deg)');
+    $textpop.css('left', '40vw');
+    $textpop.css('opacity', '1');
+    $textpop.css('z-index', '0');
+  });
+
+  $roto.mouseleave(function() {
+    $(this).css('transform', 'rotate(0deg)');
+    $textpop.css('left','20vw');
+    $textpop.css('opacity','0');
+    $textpop.css('z-index', '-1');
   });
 
 });
