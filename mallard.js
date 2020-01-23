34241432662b1047f48a7f41fb269e78b872d733
nickflores73 <46428751+nickflores73@users.noreply.github.com>
2020-01-23 09:04:44 -0800

    Add files via upload

--- mallard.js
+++ mallard.js
@@ -8,8 +8,13 @@ $(document).ready(function(){
   $roto = $('.roto');
   $geared = $('.geared');
   $textpop = $('.textpop');
+  $hammered = $('.hammered');
+  $anchor = $('.anchor');
+  $textpoop = $('.textpoop');
 
   $geared.css('transform', 'scale(1,1)');
+  $hammered.css('transform', 'scale(1,1)');
+
   $roto.mouseenter(function() {
     $(this).css('transform', 'rotate(90deg)');
     $textpop.css('left', '40vw');
@@ -19,9 +24,23 @@ $(document).ready(function(){
 
   $roto.mouseleave(function() {
     $(this).css('transform', 'rotate(0deg)');
-    $textpop.css('left','20vw');
+    $textpop.css('left','30vw');
     $textpop.css('opacity','0');
     $textpop.css('z-index', '-1');
   });
+
+ $anchor.mouseenter(function() {
+   $(this).css('transform','rotate(-45deg)');
+   $textpoop.css('right','33vw');
+   $textpoop.css('opacity','1');
+   $textpoop.css('z-index','0');
+ });
+
+ $anchor.mouseleave(function() {
+   $(this).css('transform','rotate(0deg)');
+   $textpoop.css('right','12vw');
+   $textpoop.css('opacity','0');
+   $textpoop.css('z-index','-1');
+ });
 
 });
